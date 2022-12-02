27ca4542b07065bcf33a07fd320dd56b08f989a0
Hari <kaurisanselva@gmail.com>
2022-12-01 22:40:12 -0500

    Comments

--- mte544_lab3/MovementController.py
+++ mte544_lab3/MovementController.py
@@ -11,11 +11,7 @@ from std_msgs.msg import String
 
 from math import *
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-
-from .a_star_skeleton1 import astar ##Check
-#from a_star.py import astar
 
 class movementPublisher(Node):
 
@@ -33,14 +29,15 @@ class movementPublisher(Node):
         self.subscription = self.create_subscription(Odometry,'/odom',self.pose_update,10)
         self.subscription  # prevent unused variable warning
         
-        #Initialize variables. Set current position to be position 1
-        self.curr_pose = Pose()
-        self.path = path
+        #Initialize variables
+        self.curr_pose = Pose() #using turlesim pose for convenience 
+        self.path = path # getting the path from global path
         self.count = 0
         self.reached=False
         self.facing1stpoint=False
 
     #https://gist.github.com/salmagro/2e698ad4fbf9dae40244769c5ab74434/?fbclid=IwAR0BBayKAjpEEmITIPtteVInc6GgKSd1SAGdO8IyY-cCVw2EckvFH1uXkok
+    # function to convert from quaternion to euler, found on github.
     def euler_from_quaternion(self, x,y,z,w):
         """
         Converts quaternion (w in last place) to euler roll, pitch, yaw
@@ -59,11 +56,10 @@ class movementPublisher(Node):
         cosy_cosp = 1 - 2 * (y * y + z * z)
         yaw = np.arctan2(siny_cosp, cosy_cosp)
 
-        return yaw
+        return yaw # using only yaw as that is what we need
 
     def pose_update(self, data):
         #Update the current pose value
-        #self.curr_pose=data
         self.curr_pose.x = round(data.pose.pose.position.x, 4)
         self.curr_pose.y = round(data.pose.pose.position.y, 4)
         self.curr_pose.theta = self.euler_from_quaternion(data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w)
@@ -92,7 +88,7 @@ class movementPublisher(Node):
     def ang_velocity_calculator(self, x, y):
         #Velocity calculation using proportional gain, kw
         #Gain found through experimentation.
-        kw = 0.5
+        kw = 5
         
         #Angular velocity is just the gain times the required angle, this allows for the turtle to 
         #turn quickly when its facing the wrong direction and slow down when its nearing the right 
@@ -102,12 +98,15 @@ class movementPublisher(Node):
     def move (self):
         vel_msg = Twist()
         ang_tolerance = 0.5
-        dist_tolerance = 0.15
+        dist_tolerance = 0.1
         end_dist_tolerance = 0.05
         lin_speed = 0.1
-        if (not self.reached):
-            if (self.count == 0):
+
+        if (not self.reached): # Only run if robot isn't at end goal
+            if (self.count == 0): # If at starting point
                 ang_dist = abs(self.angle_calculator(self.path[self.count][0], self.path[self.count][1]) - self.curr_pose.theta)
+
+                # Start by rotating to face the first point to navigate to.
                 if (ang_dist >= ang_tolerance and not self.facing1stpoint):
                     vel_msg.angular.z = self.ang_velocity_calculator(self.path[self.count][0], self.path[self.count][1])
                     self.velocity_publisher.publish(vel_msg)
@@ -115,10 +114,14 @@ class movementPublisher(Node):
                 else:
                     vel_msg.angular.z = 0.0
                     self.velocity_publisher.publish(vel_msg)
-                    if (not self.facing1stpoint):
+
+                    if (not self.facing1stpoint): #Only print Facing first point once
                         self.facing1stpoint=True
                         print("Facing first point")
+
                     lin_dist = self.distance_calculator(self.path[self.count][0], self.path[self.count][1])
+
+                    # Move to first point, with constant linear velocity and adjust heading if needed. Stop when within a certain distance.
                     if (lin_dist >= dist_tolerance):
                         vel_msg.linear.x = lin_speed
                         vel_msg.angular.z = self.ang_velocity_calculator(self.path[self.count][0], self.path[self.count][1])
@@ -130,8 +133,12 @@ class movementPublisher(Node):
                         self.count += 1
                         print("Reached First point")
                         print(self.curr_pose)
+            
+            # Travel to all points from second point to second last point
             elif (self.count != len(self.path)-1):
                 lin_dist = self.distance_calculator(self.path[self.count][0], self.path[self.count][1])
+
+                # Move to point, with constant linear velocity and adjust heading if needed. Stop when within a certain distance.
                 if (lin_dist >= dist_tolerance):
                     vel_msg.linear.x = lin_speed
                     vel_msg.angular.z = self.ang_velocity_calculator(self.path[self.count][0], self.path[self.count][1])
@@ -144,10 +151,17 @@ class movementPublisher(Node):
                     print (self.count)
                     print("Reached point", self.count)
                     print(self.curr_pose)
+            
+            # Travel to end goal
             else:
                 lin_dist = self.distance_calculator(self.path[self.count][0], self.path[self.count][1])
+
+                # Move to point, with constant linear velocity and adjust heading if needed. Start slowing down when near goal and stop when within a certain distance.
                 if (lin_dist >= end_dist_tolerance):
+                    # Calculate speed based on distance
                     vel_msg.linear.x = self.lin_velocity_calculator(self.path[self.count][0], self.path[self.count][1])
+
+                    # If calculated speed is greater than constant linear speed, then stay at constant linear speed
                     if (vel_msg.linear.x > lin_speed):
                         vel_msg.linear.x = lin_speed
                     vel_msg.angular.z = self.ang_velocity_calculator(self.path[self.count][0], self.path[self.count][1])
@@ -164,15 +178,26 @@ class movementPublisher(Node):
 def main(args=None):
     rclpy.init(args=args)  
 
+    # Translations to transform the /odom frame to global frame
+    x_translation_tf = -1.9999410465519822
+    y_translation_tf = -0.5000009955674611
+
+    # Open the text file with path coordinates and add to a list
     global path
-    #path = np.asarray( astar(maze, start, end), dtype=np.float)
-    #print(path)
-    
-    #path = [[-1.999938901551247, -0.5000009993037614],[-1.499938901551247, -0.5000009993037614], [-1.137920318878453,-1.8473295208778042]]
-    path = [[-1.999938901551247, -0.9000009993037614],[-1.799938901551247, -1.0000009993037614],[-1.599938901551247, -1.3000009993037614],[-1.299938901551247, -1.6000009993037614],[-1.099938901551247, -1.8000009993037614]]
-    #path = [[-1.499938901551247, -0.5000009993037614], [-1.099938901551247, -0.5000009993037614],[-0.599938901551247, -0.5000009993037614], [-0.299938901551247, -0.9000009993037614]]
-    #path = [[-1.299938901551247, -0.5000009993037614], [-1.099938901551247, -0.5000009993037614],[-0.599938901551247, -0.5000009993037614]]
-    #path = [[-1.137920318878453,-1.8473295208778042], [-1.,-2.8473295208778042]]
+    s = open("/home/hari/lab3/src/mte544_lab3/mte544_lab3/path.txt").read()
+    path=[]
+    for pair in s.split("\n"):
+        p = pair.split(" ")
+        x = float(p[0])+x_translation_tf
+        y = float(p[1])+y_translation_tf
+        path.append([x, y])
+
+    # Hard coded Paths for testing purposes
+    # path = [[-1.999938901551247, -0.5000009993037614],[-1.499938901551247, -0.5000009993037614], [-1.137920318878453,-1.8473295208778042]]
+    # path = [[-1.999938901551247, -0.9000009993037614],[-1.799938901551247, -1.0000009993037614],[-1.599938901551247, -1.3000009993037614],[-1.299938901551247, -1.6000009993037614],[-1.099938901551247, -1.8000009993037614]]
+    # path = [[-1.499938901551247, -0.5000009993037614], [-1.099938901551247, -0.5000009993037614],[-0.599938901551247, -0.5000009993037614], [-0.299938901551247, -0.9000009993037614]]
+    # path = [[-1.299938901551247, -0.5000009993037614], [-1.099938901551247, -0.5000009993037614],[-0.599938901551247, -0.5000009993037614]]
+    # path = [[-1.137920318878453,-1.8473295208778042], [-1.,-2.8473295208778042]]
     Turtlemove = movementPublisher() 
     rclpy.spin(Turtlemove)
 
--- mte544_lab3/a_star_costmap.py
+++ mte544_lab3/a_star_costmap.py
@@ -12,12 +12,15 @@ class getCostMap (Node):
     def __init__(self):
         super().__init__('getCostMap')
         self.get_logger().info('getCostMap started successfully. Starting.') 
+        # Set QoS profile
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
             durability=QoSDurabilityPolicy.TRANSIENT_LOCAL,
             history=QoSHistoryPolicy.KEEP_LAST,
             depth=10
         )
+
+        # Subscribe to cost map
         self.subscription = self.create_subscription(OccupancyGrid,'/global_costmap/costmap',self.costmap,qos_profile=qos_profile)
         self.subscription  # prevent unused variable warning
         self.data = []
@@ -26,70 +29,72 @@ class getCostMap (Node):
     def costmap (self, data):
         if (not self.gotmap):
             self.data = data.data
-            print("hi")
+            print("hi") # To check if the code gets to this point
             self.resolution = 0.05 #meters/pixel
-            self.origin = np.array([-5.75, -3.72])
-            self.maze = np.empty((data.info.height, data.info.width), int)
-            self.maze = np.reshape(self.data, (-1, data.info.width))
-            scale_percent = 25 # percent of original size
+            self.origin = np.array([-5.75, -3.72]) # Translation of map to map frame
+            self.maze = np.empty((data.info.height, data.info.width), int) # 1D array in row-major format
+            self.maze = np.reshape(self.data, (-1, data.info.width)) # Reshape into 2D array with same dimensions as map
+            scale_percent = 5 # percent of original size to scale the array down to reduce computation
+
+            # Scaled dimensions, keeping aspect ratio intact
             width = int(data.info.width* scale_percent / 100)
             height = int(data.info.height * scale_percent / 100)
             dim = (width, height)
             
-            # resize image
+            # resize map/occupancy grid
             self.maze = cv2.resize(self.maze.astype('float32'), dim, interpolation = cv2.INTER_AREA)
 
             #Find Occupancy Grid from Cost Map
             self.maze[self.maze < 63] = 0 # Free space
             self.maze[self.maze > 62] = 1 # Occupied Space
             
+            # For display the map if need be
             # img = self.maze
             # self.maze[self.maze > 0] = 205
             # cv2.imshow("maze", img)
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
 
-            # x_index * resolution + x_origin = cart_x_coordinate
-            # (cart_x_coordinate - x_origin) / resolution = x_index
-            # Define here your start and end points
-            # start_cart = np.array([0, -0.08])
-            # start = (np.rint((start_cart - self.origin)/(self.resolution / (scale_percent / 100)))).astype(int)
-            # start = start.tolist()
-            # end = (6, 6)
+            # index * resolution + origin = cart_coordinate
+            # (cart_coordinate - x_origin) / resolution = index
+            # Determining the start and end coordinates by transforming it from global frame to pixel/array indices using the second equation above.
+            start_cart = np.array([0, -0.08])
+            start = (np.rint((start_cart - self.origin)/(self.resolution / (scale_percent / 100)))).astype(int)
+            start[1]=(height-1)-start[1]
+            start = start.tolist()
+            end_cart = np.array([3, 1.9])
+            end = (np.rint((end_cart - self.origin)/(self.resolution / (scale_percent / 100)))).astype(int)
+            end[1]=(height-1)-end[1]
+            end = end.tolist()
 
-            start = (29,14)
-            end = (36, 0)
-            print (self.maze[start[0]][start[1]])
-            print (self.maze[end[0]][end[1]])
+            # Print value of start and end points to check it is free space (aka 0) so that A* works
+            print (self.maze[start[1]][start[0]])
+            print (self.maze[end[1]][end[0]])
                         
-            # Compute the path with your implementation of Astar
+            # Compute the path with implementation of A#
             path = np.asarray( astar(self.maze, start, end), dtype=float)
-            path = path * self.resolution + self.origin
-            # my_df = pd.DataFrame(path)
-            # my_df.to_csv('path.csv',header = False, index= False)
 
+            # Convert the path to global frame
+            path = path * (self.resolution/(scale_percent/100)) + self.origin
+            
+            # Write the path coordinatees to a .txt file to be imported into movement controller script later
+            with open ('/home/hari/lab3/src/mte544_lab3/mte544_lab3/path.txt', 'w') as f:
+                f.write(path)
+
+            #Set true so it only runs once
             self.gotmap = True
-        # maze_plot=np.transpose(np.nonzero(self.maze))
-
-        # plt.plot(maze_plot[:,0], maze_plot[:,1], 'o')
-        
-        # if not np.any(path): # If path is empty, will be NaN, check if path is NaN
-        #     print("No path found")
-        # else:
-        #     plt.plot(path[:,0], path[:,1])
-        # plt.grid()
-        # plt.show()
 
 class node():
     """A node class for A* Pathfinding"""
 
+    # Initalizes a node with its parent node (aka where it came from, its current position, and its costs)
     def __init__(self, parent=None, position=None):
         self.parent = parent
         self.position = position
 
-        self.g = 0
-        self.h = 0
-        self.f = 0
+        self.g = 0 # Cost
+        self.h = 0 # Heuristic
+        self.f = 0 # Cost + Heuristic
 
     def __eq__(self, other):
         return self.position == other.position
@@ -116,6 +121,7 @@ def astar(maze, start, end):
         # Get the current node
         current_node = open_list[0]
         current_index = 0
+        #Finding the node in the open list with the lowest cost, which is will be the current node
         for index, item in enumerate(open_list):
             if item.f < current_node.f:
                 current_node = item
@@ -126,17 +132,19 @@ def astar(maze, start, end):
         closed_list.append(current_node)
 
         # Found the goal, you can also implement what should happen if there is no possible path
-        if current_node == end_node:
-            # Complete here code to return the shortest path found
+        if current_node.position == end_node.position:
             path = []
             current = current_node
+
+            # Trace through the closed list to find the path from the end node to the start node
             while current is not None:
                 path.append(current.position)
                 current = current.parent
             return path[::-1]
 
-        # Complete here code to generate children, which are the neighboring nodes. You should use 4 or 8 points connectivity for a grid.
+        # Generate children, which are the neighboring nodes. Uses 8 points connectivity for a grid.
         children = []
+        #Neighbors in the following directions: South, North, West, East, South-West, North-West, South-East, North-West
         neighbours = [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]
         for single_neighbour in neighbours: # Adjacent squares
             # Get node position
@@ -147,7 +155,8 @@ def astar(maze, start, end):
                (0 > node_neighbour[1] or node_neighbour[1] > (len(maze[len(maze)-1]) -1)) or
                maze[node_neighbour[0]][node_neighbour[1]] != 0
             ):
-                continue        
+                continue # Go to next neighbour if this neighbour is invalid
+            #if valid
             # Create new node
             new_node = node(current_node, node_neighbour)
             # Append
@@ -160,7 +169,7 @@ def astar(maze, start, end):
                 if child == closed_child:
                     break
             else:
-                # Create the f, g, and h values, replace the 0s with appropriate formulations of the costs
+                # Calculate the cost only if not in closed list
                 child.g = current_node.g + 1
                 child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
                 child.f = child.g + child.h
@@ -168,24 +177,50 @@ def astar(maze, start, end):
                 # Complete here code to check whether to add a child to the open list
                 open_list.append(child)
 
+                # for open_child in open_list:
+                #     if child.position == open_child.position and child.f<open_child.f:
+                #         open_child=child
+                #         break
+                #     elif child.position == open_child.position and child.f>open_child.f:
+                #         break
+                # else:
+                #     # Add current child to list only if not already in open list
+                #     open_list.append(child)
+
 def main(args=None):
     rclpy.init(args=args)  
     map = getCostMap()
-    # Load your maze here
-    
-    # print (maze)
-    # This is an example maze you can use for testing, replace it with loading the actual map
-    # maze = [[0,   0,   0,   0,   1,   0, 0, 0, 0, 0],
-            # [0, 0.8,   1,   0,   1,   0, 0, 0, 0, 0],
-            # [0, 0.9,   1,   0,   1,   0, 1, 0, 0, 0],
-            # [0,   1,   0,   0,   1,   0, 1, 0, 0, 0],
-            # [0,   1,   0,   0,   1,   0, 0, 0, 0, 0],
-            # [0,   0,   0, 0.9,   0,   1, 0, 0, 0, 0],
-            # [0,   0, 0.9,   1,   1, 0.7, 0, 0, 0, 0],
-            # [0,   0,   0,   1,   0,   0, 0, 0, 0, 0],
-            # [0,   0,   0,   0, 0.9,   0, 0, 0, 0, 0],
-            # [0,   0,   0,   0,   0,   0, 0, 0, 0, 0]]
 
+    # # This is an example maze you can use for testing, replace it with loading the actual map
+    # maze = [[0,   0,   0,   0,   1,   0, 0, 0, 0, 0],
+    #         [0, 0.8,   1,   0,   1,   0, 0, 0, 0, 0],
+    #         [0, 0.9,   1,   0,   1,   0, 1, 0, 0, 0],
+    #         [0,   1,   0,   0,   1,   0, 1, 0, 0, 0],
+    #         [0,   1,   0,   0,   1,   0, 0, 0, 0, 0],
+    #         [0,   0,   0, 0.9,   0,   1, 0, 0, 0, 0],
+    #         [0,   0, 0.9,   1,   1, 0.7, 0, 0, 0, 0],
+    #         [0,   0,   0,   1,   0,   0, 0, 0, 0, 0],
+    #         [0,   0,   0,   0, 0.9,   0, 0, 0, 0, 0],
+    #         [0,   0,   0,   0,   0,   0, 0, 0, 0, 0]]
+
+    
+    
+    # # Define here your start and end points
+    # start = (0, 0)
+    # end = (6, 6)
+    
+    # # Compute the path with your implementation of Astar
+    # path = np.asarray( astar(maze, start, end), dtype=float)
+    # maze_plot=np.transpose(np.nonzero(maze))
+
+    # plt.plot(maze_plot[:,0], maze_plot[:,1], 'o')
+    
+    # if not np.any(path): # If path is empty, will be NaN, check if path is NaN
+    #     print("No path found")
+    # else:
+    #     plt.plot(path[:,0], path[:,1])
+    # plt.grid()
+    # plt.show()
     
 
     rclpy.spin(map)
